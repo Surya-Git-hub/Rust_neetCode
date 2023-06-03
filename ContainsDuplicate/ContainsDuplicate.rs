77857393bfa7f1452877e9654f3ccde2be2e12a0
surya kant <suryakant0013@yahoo.in>
2023-06-03 23:19:42 +0530

    setup

---
+++ ContainsDuplicate/ContainsDuplicate.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        let mut map = HashSet::new();
+
+        for &n in nums.iter() {
+            if map.contains(&n) {
+                return true;
+            }
+
+            map.insert(n);
+        }
+
+        false
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+// use crate::ContainsDuplicate::ContainsDuplicate::contains_duplicate;
+fn main() {
+    println!("Hello, world!");
+    // println!("{}",contains_duplicate([1,2,44,3,5,2,6]))
+}
