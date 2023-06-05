cb9805dd9ecb3505152e4e597126bf9e79e01e0e
surya kant <suryakant0013@yahoo.in>
2023-06-06 01:02:29 +0530

    better two sum implementation

--- src/twoSum.rs
+++ src/twoSum.rs
@@ -1,27 +1,19 @@
+use std::collections::HashMap;
+
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut i: i32 = 0;
-        let mut j: i32 = 0;
-        let numlen = nums.len() as i32;
-
-
-
-        loop {
-            if i >= numlen {
-                return vec![];
+        let mut map = HashMap::new();
+        
+        for (i, n) in nums.into_iter().enumerate(){
+            let diff = target - n;
+            
+            if let Some(&j) = map.get(&diff){
+                return vec![i as i32, j as i32];
+            }else{
+                map.insert(n, i);
             }
-
-            for j in (i + 1)..numlen {
-                if j >= numlen {
-                    return vec![];
-                }
-
-                if nums[i as usize] + nums[j as usize] == target {
-                    return vec![i, j];
-                }
-            }
-
-            i += 1;
         }
+        
+        unreachable!()
     }
 }
